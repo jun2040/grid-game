a03ff8cf4309417dc4a01d6c6969a6066e478327
bkjn2040 <bkjn415@gmail.com>
2024-12-06 13:36:02 +0100

    Add obstacle entity

---
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Obstacle.java
@@ -0,0 +1,49 @@
+package ch.epfl.cs107.icoop.actor;
+
+import ch.epfl.cs107.play.areagame.actor.AreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.engine.actor.Sprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+import ch.epfl.cs107.play.window.Canvas;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Obstacle extends AreaEntity {
+    private final Sprite sprite;
+
+    /**
+     * Default AreaEntity constructor
+     *
+     * @param area        (Area): Owner area. Not null
+     * @param orientation (Orientation): Initial orientation of the entity in the Area. Not null
+     * @param position    (DiscreteCoordinate): Initial position of the entity in the Area. Not null
+     */
+    public Obstacle(Area area, Orientation orientation, DiscreteCoordinates position) {
+        super(area, orientation, position);
+
+        this.sprite = new Sprite("rock.2", 1, 1, this);
+    }
+
+    @Override
+    public void draw(Canvas canvas) { sprite.draw(canvas); }
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public boolean takeCellSpace() { return true; }
+
+    @Override
+    public boolean isCellInteractable() { return true; }
+
+    @Override
+    public boolean isViewInteractable() { return true; }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {}
+}
