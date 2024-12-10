ba2b18657a37a20484ee0078e2411671a6cccbfb
bkjn2040 <bkjn415@gmail.com>
2024-12-10 21:15:34 +0100

    Add abstract Projectile class

---
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Projectile.java
@@ -0,0 +1,91 @@
+package ch.epfl.cs107.icoop.actor;
+
+import ch.epfl.cs107.play.areagame.actor.Interactable;
+import ch.epfl.cs107.play.areagame.actor.Interactor;
+import ch.epfl.cs107.play.areagame.actor.MovableAreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+
+import java.util.Collections;
+import java.util.List;
+
+public abstract class Projectile extends MovableAreaEntity implements Interactor {
+    private static int MOVE_DURATION = 10;
+
+    private final int speed;
+    private int range;
+
+    /**
+     * Default MovableAreaEntity constructor
+     *
+     * @param area        (Area): Owner area. Not null
+     * @param orientation (Orientation): Initial orientation of the entity. Not null
+     * @param position    (Coordinate): Initial position of the entity. Not null
+     */
+    public Projectile(Area area, Orientation orientation, DiscreteCoordinates position, int speed, int range) {
+        super(area, orientation, position);
+
+        this.speed = speed;
+        this.range = range;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+
+        move(MOVE_DURATION / speed);
+        range--;
+
+        if (range < 0)
+            destroy();
+    }
+
+    public void destroy() {
+        unregister();
+    }
+
+    private void unregister() {
+        getOwnerArea().unregisterActor(this);
+    }
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public List<DiscreteCoordinates> getFieldOfViewCells() {
+        return List.of();
+    }
+
+    @Override
+    public boolean wantsCellInteraction() {
+        return true;
+    }
+
+    @Override
+    public boolean wantsViewInteraction() {
+        return false;
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return false;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return false;
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+    }
+}
