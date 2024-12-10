12b33ddc2660027f7c994612ade748c4d8cc7561
bkjn2040 <bkjn415@gmail.com>
2024-12-10 01:29:27 +0100

    Add pressure plate area entity

--- iccoop/src/main/java/ch/epfl/cs107/icoop/actor/PressurePlate.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/PressurePlate.java
@@ -1,4 +1,79 @@
 package ch.epfl.cs107.icoop.actor;
 
-public class PressurePlate {
+import ch.epfl.cs107.icoop.handler.ICoopInteractionVisitor;
+import ch.epfl.cs107.play.areagame.actor.AreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.engine.actor.RPGSprite;
+import ch.epfl.cs107.play.engine.actor.Sprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.signal.logic.Logic;
+import ch.epfl.cs107.play.window.Canvas;
+
+import static ch.epfl.cs107.play.math.Orientation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PressurePlate extends AreaEntity implements Logic {
+    private boolean isPressed = false;
+
+    private Sprite plateSprite;
+
+    /**
+     * Default AreaEntity constructor
+     *
+     * @param area        (Area): Owner area. Not null
+     * @param position    (DiscreteCoordinate): Initial position of the entity in the Area. Not null
+     */
+    public PressurePlate(Area area, DiscreteCoordinates position) {
+        super(area, DOWN, position);
+
+        this.plateSprite = new RPGSprite("GroundPlateOff", 1, 1, this);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        plateSprite.draw(canvas);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+    }
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return false;
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+        ((ICoopInteractionVisitor) v).interactWith(this, isCellInteraction);
+    }
+
+    @Override
+    public boolean isOn() {
+        return isPressed;
+    }
+
+    @Override
+    public boolean isOff() {
+        return !isPressed;
+    }
 }
