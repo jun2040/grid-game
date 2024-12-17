520f059167e65a815f55ef287d20ad7b01026ead
bkjn2040 <bkjn415@gmail.com>
2024-12-17 23:35:34 +0100

    Setup Arena with keys and teleporter

--- iccoop/src/main/java/ch/epfl/cs107/icoop/area/maps/Arena.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/area/maps/Arena.java
@@ -1,9 +1,14 @@
 package ch.epfl.cs107.icoop.area.maps;
 
+import ch.epfl.cs107.icoop.actor.ElementType;
+import ch.epfl.cs107.icoop.actor.Key;
+import ch.epfl.cs107.icoop.actor.Teleporter;
 import ch.epfl.cs107.icoop.area.ICoopArea;
 import ch.epfl.cs107.play.engine.actor.Background;
 import ch.epfl.cs107.play.engine.actor.Foreground;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+
+import static ch.epfl.cs107.play.math.Orientation.*;
 
 public class Arena extends ICoopArea {
     public static DiscreteCoordinates[] ARRIVAL_POINTS =
@@ -13,6 +18,22 @@ public class Arena extends ICoopArea {
     protected void createArea() {
         registerActor(new Background(this));
         registerActor(new Foreground(this));
+
+        Key fireKey = new Key(this, UP, new DiscreteCoordinates(9, 16), ElementType.FIRE);
+        Key waterKey = new Key(this, UP, new DiscreteCoordinates(9, 4), ElementType.WATER);
+        Teleporter teleporter = new Teleporter(this, UP, "Spawn", Spawn.ARRIVAL_POINTS, new DiscreteCoordinates(10, 10));
+
+        teleporter.addKey(fireKey);
+        teleporter.addKey(waterKey);
+
+        registerActor(fireKey);
+        registerActor(waterKey);
+        registerActor(teleporter);
+    }
+
+    @Override
+    public boolean isViewCentered() {
+        return true;
     }
 
     @Override
