ad81d6394a8b5601cc51edf7407d9794980dcc3c
bkjn2040 <bkjn415@gmail.com>
2024-12-19 01:05:36 +0100

    Fix import

--- iccoop/src/main/java/ch/epfl/cs107/icoop/area/ICoopBehavior.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/area/ICoopBehavior.java
@@ -1,9 +1,6 @@
 package ch.epfl.cs107.icoop.area;
 
-import ch.epfl.cs107.icoop.actor.ElementalEntity;
-import ch.epfl.cs107.icoop.actor.Obstacle;
-import ch.epfl.cs107.icoop.actor.Rock;
-import ch.epfl.cs107.icoop.actor.Unstoppable;
+import ch.epfl.cs107.icoop.actor.*;
 import ch.epfl.cs107.play.areagame.actor.Interactable;
 import ch.epfl.cs107.play.areagame.area.Area;
 import ch.epfl.cs107.play.areagame.area.AreaBehavior;
